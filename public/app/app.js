574e4c37789af6c08a484258711d2f229a9f0acd
kelsy michael gagnebin <kelsymichael@gmail.com>
2015-06-10 16:52:22 -0600

    Add basic folder & file structure to public/

---
+++ public/app/app.js
@@ -0,0 +1,45 @@
+(function(){
+  'use strict';
+
+var app = angular.module('fashionphile', [ 'ui.router']);
+
+//config
+app
+	.config(function($stateProvider, $urlRouterProvider) {
+		$urlRouterProvider.when('', '/');
+		$urlRouterProvider.otherwise('/');
+		$stateProvider
+		 	.state('login', {
+		 			url: '/',
+          templateUrl : 'app/login/loginView.html',
+          controller  : 'LoginCtrl'
+      })
+      .state('selection', {
+      		url: '/selection',
+          templateUrl : 'app/selection/selectionView.html',
+          controller  : 'SelectionCtrl'
+      })
+      .state('customer', {
+          url: '/customer',
+          templateUrl : 'app/customer/customerView.html',
+          controller  : 'CustomerCtrl'
+      })
+      .state('employee', {
+          url: '/employee',
+          templateUrl : 'app/employee/employeeView.html',
+          controller  : 'EmployeeCtrl'
+      })
+      .state('stats', {
+          url: '/stats',
+          templateUrl : 'app/stats/statsView.html',
+          controller  : 'StatsCtrl'
+      })
+      .state('walldisplay', {
+          url: '/walldisplay',
+          templateUrl : 'app/wallDisplay/wallDisplayView.html',
+          controller  : 'WallDisplayCtrl'
+      });
+      
+});
+
+})();
