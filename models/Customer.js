746ec0983053f584b16d24ea53c76c402033e689
dylanlott <lott.dylan@gmail.com>
2015-06-11 10:03:41 -0600

    created customerService and added functions to CustomerCtrl

--- models/Customer.js
+++ models/Customer.js
@@ -5,8 +5,8 @@ var Schema = mongoose.Schema;
 var  customerModel = new Schema({
   name: String,
   joined: {type: Date, default: Date.now},
-  status: {type: String, enum: [pending, done]},
+  status: {type: String, enum: [pending, done], default: pending},
   helpedAt: {type: Date}
 });
 
-module.exports = mongoose.model('Customer', customerModel); 
+module.exports = mongoose.model('Customer', customerModel);
