4aaa18859c38c6c9a3398ebe664c1fe729ba8b1d
Bryan Smith <b.smith33@hotmail.com>
2015-06-11 13:56:19 -0600

    comment

--- public/app/customer/CustomerCtrl.js
+++ public/app/customer/CustomerCtrl.js
@@ -4,14 +4,10 @@
 var app = angular.module('fashionphile');
 
 app.controller('CustomerCtrl', function($scope, $q, $http, CustomerService){
-
-// $scope.getCustomers = function(){
-//   CustomerService.getCustomers(); 
-// }; 
-
 $scope.addCustomer = function($scope, name){
   console.log(customer); 
-  // CustomerService.getCustomers(); 
-}
+	};
+})
 
 })();
+
