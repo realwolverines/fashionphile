4a4574610e275b608914b163511d45bc61170eba
Bryan Smith <b.smith33@hotmail.com>
2015-06-12 11:14:29 -0600

    updating all files to be up to date

--- controllers/QueueCtrl.js
+++ controllers/QueueCtrl.js
@@ -17,30 +17,30 @@ module.exports = {
 
   getByLocation: function(req, res){
     Customer
-      .find({location: }, function(err, customers){
+      .find({location:{} }, function(err, customers){
         if(err) res.status(500).end(); 
         res.json(customers); 
       })
   },
 
-  delete: function(req, res) {
-    Customer
-      .findById(req.params.customerId)
-      .remove()
-      .exec(function(err, res){
-        console.log(res); 
-        if(err) return res.status(500).end(); 
-        return res.status(200).json(result); 
-      });
-  },
+//   delete: function(req, res) {
+//     Customer
+//       .findById(req.params.customerId)
+//       .remove()
+//       .exec(function(err, res){
+//         console.log(res); 
+//         if(err) return res.status(500).end(); 
+//         return res.status(200).json(result); 
+//       });
+//   },
 
-  update: function(req, res){
-    Customer 
-      .findByIdAndUpdate(req.params.customerId, {status: req.body.status})
-      .exec(function(err, result) {
-        console.log(result); 
-        if(err) return res.status(500).end(); 
-        return res.status(200).json(result); 
-      })
-  }
-}
+//   update: function(req, res){
+//     Customer 
+//       .findByIdAndUpdate(req.params.customerId, {status: req.body.status})
+//       .exec(function(err, result) {
+//         console.log(result); 
+//         if(err) return res.status(500).end(); 
+//         return res.status(200).json(result); 
+//       })
+//   }
+// }
