5357e7e206dc51c5e5195051dbf6348b74d8c30e
dylanlott <lott.dylan@gmail.com>
2015-06-18 16:47:17 -0600

    updated user and location models with required schema keys

--- models/Customer.js
+++ models/Customer.js
@@ -3,12 +3,12 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var  customerModel = new Schema({
-  name: String,
+  name: {type: String, required: true},
   joined: {type: Date, default: Date.now},
   status: {type: String, enum: ["pending", "done"], default: "pending"},
   helpedAt: {type: Date},
   location: {type: String},
-  email: {type: String},
+  email: {type: String, unique: true},
   active: {type: Boolean, default: true}
 });
 
