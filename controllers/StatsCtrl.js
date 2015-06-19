6b43d4fc0c54b1c98284ac1283a27d8beec01f81
dylanlott <lott.dylan@gmail.com>
2015-06-19 11:00:13 -0600

    services folder

--- controllers/StatsCtrl.js
+++ controllers/StatsCtrl.js
@@ -6,37 +6,46 @@ module.exports.getStats = function(req, res){
     statsService.getStats().then(function(stats){
         //create array for all stat objects to go into 
         var newStats = []; 
-        var length = stats.length-1; 
+        var length = stats.length-1;
+        var WaitTimesArr = []; 
 
-        /* GET AVERAGES ******************************/ 
+        /* GET AVERAGES **************************************/ 
         var sum = 0;
         stats.map(function(i){
           console.log(sum);
           sum += (i.helpedAt - i.joined);
+          WaitTimesArr.push(i.helpedAt - i.joined);
+          console.log("pushed to wait times ", (i.helpedAt - i.joined)); 
         })
         var average = sum / length; 
         console.log("total average is ", average);
 
-        //add average to array 
+        //add average to newStats array 
         newStats.push({"average": average})
         console.log(newStats);
 
+        /* LONGEST WAIT TIME  *********************/
+        var longestWait = Math.max([WaitTimesArr]); 
+        newStats.push({"longestWait": longestWait}); 
 
-        /* GET NUMBER OF DAILY CUSTOMERS ************/
+        /* GET SHORTEST WAIT TIME *****************************/
+        var shortestWait = Math.min([WaitTimesArr]); 
+        newStats.push({"shortestWait": shortestWait})
 
+
+        /* GET NUMBER OF DAILY CUSTOMERS *********************/
+
+
+        /* GET NUMBER OF DAILY CUSTOMERS *********************/
 
 
         /* GET AVERAGE NUMBER OF DAILY CUSTOMERS ************/
 
 
-
-        /* GET SHORTEST WAIT TIME *****************************/
-
-
-
         /*  GET LIFETIME CUSTOMERS *****************************/
 
 
+        console.log("stats object ", newStats); 
 
 
     });
