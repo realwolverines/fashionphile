e2322f8d692353ca3a95bb6244f649ce5e958c83
Bryan Smith <b.smith33@hotmail.com>
2015-06-19 22:37:18 -0600

    getting up to date with master

--- public/app/admin/adminCtrl.js
+++ public/app/admin/adminCtrl.js
@@ -1,13 +1,14 @@
-(function(){
-	'use strict';
+
 
 var app = angular.module('fashionphile');
 
-app.controller('adminCtrl', function($scope, adminService, stats){
+app.controller('adminCtrl', function($scope, adminService, stats, locations){
 
     $scope.stats = stats;
     $scope.averageWait = stats.averageTime; 
 
+    $scope.locations = locations;
+
   });
 
-})();
+
