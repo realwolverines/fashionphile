35f9b363bc427a020f254483e9e5064d898de5b3
dylanlott <lott.dylan@gmail.com>
2015-06-20 00:24:58 -0600

    removed unused functions

--- services/statsService.js
+++ services/statsService.js
@@ -13,33 +13,6 @@ module.exports = {
           dfd.resolve(customers);
       })
   return dfd.promise;
-  },
-
-  getLast7Days: function(req, res){
-    var dfd = q.defer(); 
-      Customer
-        .find()
-        .where("helpedAt").gt(0)
-        .exec()
-        .then(function(customers){
-          console.log(customers); 
-          dfd.resolve(customers); 
-        })
-      return dfd.promise; 
   }
-
-  // getDailyCustomers: function(req, res){
-  //   var dfd = q.defer(); 
-  //       Customer 
-  //           .find({
-  //               helpedAt: {
-  //                   $gt: ObjectId.createFromTimestamp(Date.now()*1000-12*60*60)
-  //               }
-  //           })
-  //           .then(function(customers){
-  //             console.log("getDailyCustomers ", customers); 
-  //             dfd.resolve(customers); 
-  //           })
-  // },
 
 }
