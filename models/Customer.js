a31a8fa51c422ffaa4c1bb59c0a3f89ea47ec9d4
Bryan Smith <b.smith33@hotmail.com>
2015-06-22 21:51:09 -0600

    fixed up store stats

--- models/Customer.js
+++ models/Customer.js
@@ -5,7 +5,7 @@ var moment = require('moment');
 
 var  customerModel = new Schema({
   name: {type: String},
-  joined: {type: Date},
+  joined: {type: Date, default: Date.now},
   status: {type: String, enum: ["pending", "done"], default: "pending"},
   helpedAt: {type: Date},
   location: {type: String},
